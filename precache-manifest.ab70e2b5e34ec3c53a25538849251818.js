7824e9d3658504ac4243ad78dba777ab0b9115e5
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-04-09 17:44:54 -0400

    Updates

---
+++ precache-manifest.ab70e2b5e34ec3c53a25538849251818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049167e6e8ec06c033541f6d10544726",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "1eec55f2ba685800f8fc",
+    "url": "/would-you-rather/static/css/2.cd764fe0.chunk.css"
+  },
+  {
+    "revision": "d3228a35edf018762a93",
+    "url": "/would-you-rather/static/css/main.584e4000.chunk.css"
+  },
+  {
+    "revision": "1eec55f2ba685800f8fc",
+    "url": "/would-you-rather/static/js/2.d8390bf5.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/would-you-rather/static/js/2.d8390bf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3228a35edf018762a93",
+    "url": "/would-you-rather/static/js/main.2d753e1e.chunk.js"
+  },
+  {
+    "revision": "c36b83d95e7efdc10c78",
+    "url": "/would-you-rather/static/js/runtime-main.d93197ee.js"
+  }
+]);
