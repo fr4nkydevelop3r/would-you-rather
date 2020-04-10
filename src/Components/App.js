6721f9ed7ccdf8f89133863798b101acb5be1973
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-04-10 19:02:48 -0400

    feat: update header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import PollItem from "./PollItem";
 import CreatePoll from "./CreatePoll";
